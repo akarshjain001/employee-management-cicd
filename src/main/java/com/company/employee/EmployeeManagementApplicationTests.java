6cb6a31fcabc36a3edfc39c2a5efd7770e046908
Akarsh Jain <57222530+akarshjain001@users.noreply.github.com>
2026-07-10 02:09:57 +0530

    Add unit test for EmployeeManagementApplication

---
+++ src/main/java/com/company/employee/EmployeeManagementApplicationTests.java
@@ -0,0 +1,14 @@
+package com.company.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
